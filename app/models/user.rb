5676811603031abb64d73038c156ff5a07f5c0a4
kakipp <75791004+kakipp@users.noreply.github.com>
2021-03-20 16:15:21 +0900

    フロント画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+         belongs_to :affiliation
+         belongs_to :position
+
+
+         with_options presence: true do
+           validates :name
+           validates :introduction
+           validates :position_id, numericality: { other_than: 1 }
+           validates :affiliation_id, numericality: { other_than: 1 }
+         end
 end
